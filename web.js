47a08561ec1148179bbbad525527c3fe7fab8cd8
Tung Hoang <tunghoang2011@gmail.com>
2023-05-20 01:29:14 +0700

    first

--- web.js
+++ web.js
@@ -2,13 +2,13 @@ const express=require('express');
 var router = express.Router();
 const path = require('path');
 
-router.get('/js-vnm',function(req,res){
-    res.sendFile(path.join(__dirname+'/www/js/index.js'));
+router.get('/jsvnm',function(req,res){
+    res.sendFile(path.join(__dirname+'/www/js/index-vnm.js'));
     //__dirname : It will resolve to your project folder.
 });
 
 router.get('/',function(req,res){
-    res.sendFile(path.join(__dirname+'/www/index.html'));
+    res.sendFile(path.join(__dirname+'/www/index-vnm.html'));
     //__dirname : It will resolve to your project folder.
 });
 
