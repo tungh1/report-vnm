db0e2aca3e8a20629726a100c05ad4cc3224bd5b
Tung Hoang <tunghoang2011@gmail.com>
2023-05-21 00:06:50 +0700

    add brand

--- api0.js
+++ api0.js
@@ -59,26 +59,28 @@ router.get('/otp/report', async (req, res) => {
   const filter_huawei = {timeStamp: timeStamp.getTime(), brand: 'Huawei'};
   const filter_garmin = {timeStamp: timeStamp.getTime(), brand: 'Garmin'};
   const filter_viber = {timeStamp: timeStamp.getTime(), brand: 'Viber'};
+  const filter_telegram = {timeStamp: timeStamp.getTime(), brand: 'Telegram'};
 
   //const data = await OtpModel.aggregate().sortByCount("brand");
   const lstOtp = await OtpModel.count(filter);
-  const count_linkedin_tha = await OtpModel.count(filter_linkedin);
-  const count_line_tha = await OtpModel.count(filter_line);
-  const count_bigo_tha = await OtpModel.count(filter_bigo);
-  const count_discord_tha = await OtpModel.count(filter_discord);
-  const count_grab_tha = await OtpModel.count(filter_grab);
-  const count_agoda = await OtpModel.count(filter_agoda);
+  // const count_linkedin_tha = await OtpModel.count(filter_linkedin);
+  // const count_line_tha = await OtpModel.count(filter_line);
+  // const count_bigo_tha = await OtpModel.count(filter_bigo);
+  // const count_discord_tha = await OtpModel.count(filter_discord);
+  // const count_grab_tha = await OtpModel.count(filter_grab);
+  // const count_agoda = await OtpModel.count(filter_agoda);
   const count_apple = await OtpModel.count(filter_apple);
   const count_kucoin = await OtpModel.count(filter_kucoin);
-  const count_huawei = await OtpModel.count(filter_huawei);
-  const count_garmin = await OtpModel.count(filter_garmin);
-  const count_viber = await OtpModel.count(filter_viber);
+  //const count_huawei = await OtpModel.count(filter_huawei);
+  //const count_garmin = await OtpModel.count(filter_garmin);
+  //const count_viber = await OtpModel.count(filter_viber);
   const count_microsoft = await OtpModel.count(filter_microsoft);
+  const count_telegram = await OtpModel.count(filter_telegram);
   
   var result = "<p id='farm0' total='" + lstOtp + "'><b>Total</b> (" + current_date + "-" + timeStamp.getTime() + "): " + lstOtp + " ^tp</b></br>";
   // result += "<b style='color:blue'>LinkedIn: <span id='linkedin_0'>" + count_linkedin_tha + "</span></b></br>";
-  // result += "<b style='color:blue'>Apple: <span id='apple_0'>" + count_apple + "</span></b></br>";
-  result += "Microsoft: " + count_microsoft + "</p>";
+  result += "<b style='color:blue'>Apple: <span id='apple_0'>" + count_apple + "</span></b></br>";
+  result += "Microsoft: " + count_microsoft + "</br>";
   // result += "Line: " + count_line_tha + "</br>";
   // result += "Bigo: " + count_bigo_tha + "</br>";
   // result += "Discord: " + count_discord_tha + "</br>";
@@ -86,8 +88,8 @@ router.get('/otp/report', async (req, res) => {
   // result += "Agoda: " + count_agoda + "</br>";
   // result += "Garmin: " + count_garmin + "</br>";
   // result += "Viber: <span id='viber_0'>" + count_viber + "</span></br>";
-  // result += "Kucoin: <span id='kucoin_0'>" + count_kucoin + "</span></br>";
-  // result += "Huawei: " + count_huawei + "</p>";
+  result += "Kucoin: <span id='kucoin_0'>" + count_kucoin + "</span></br>";
+  result += "Telegram: " + filter_telegram + "</p>";
 
   res.send(result);
   
